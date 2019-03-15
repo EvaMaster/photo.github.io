e733698a0db6a59235d135747f933ce6021646cb
EvaMaster <36130525+EvaMaster@users.noreply.github.com>
2019-03-15 19:23:03 +0200

    2
    2

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # photo.github.io
+
+/*https://www.figma.com/file/aswQCTxrLXMwiSbUn6sMrK/Displays-new-(Copy)?node-id=28%3A0*/
--- main.js
+++ main.js
@@ -1,43 +1,80 @@
 $(document).ready(function(){
     $(".album-photo").magnificPopup({
         type:"image",
-        removalDelay: 300,
+        removalDelay: 1000,
+        mainClass: 'my-mfp-zoom-in',
+        
     });
+    
+    
+    $('.open-popup-link').magnificPopup({
+  type:'inline',
+  midClick: true,
+        removalDelay: 1000,
+        mainClass: 'my-mfp-zoom-in'
 });
 
+   $('.video-play').magnificPopup({
+  items: {
+	     src: 'https://www.youtube.com/watch?v=7eo8XpT4CmM'
+     },
+  type: 'iframe',
+  iframe: {
+	    	markup: '<div class="mfp-iframe-scaler">'+
+            		'<div class="mfp-close"></div>'+
+            		'<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
+            		'</div>', 
+        patterns: {
+            youtube: {
+	              index: 'youtube.com/', 
+	              id: 'v=', 
+	              src: 'https://www.youtube.com/embed/2xDgGCDwRFc' 
+		        }
+		     },
+		     srcAction: 'iframe_src', 
+     }
+}); 
+  
+  $(".video-button").click(function(){
+      $(".video_hidden").show("slow");
+  });
+$(".video-button_hidden").click(function(){
+      $(".video_hidden").hide("slow");
+  });
+    
+    
+   
+    $(".album-button").click(function(){
+      $(".album-list_hidden").show("slow");
+  });
+$("#album-button_close").click(function(){
+      $(".album-list_hidden").hide("slow");
+  });
+    
+    
+    $(".content-wrapper-header").click(function(){
+      $(".services-wrapper-menu").show("slow");
+  });
+    
+    
+    
+    
+    
+    });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*const album_btn = document.getElementById("album-button"),
+const album_btn = document.getElementById("album-button"),
       album_btn_close = document.getElementById("album-button_close"),
-      photo = document.getElementsByClassName("album-photo"),
-      album_hidden = document.getElementsByName("album"),
-      galery_hidden = document.getElementsByClassName("galery-hidden")[0],
-      galery_hidden_photo = document.getElementsByClassName("galery-hidden_photo");
+      video_btn = document.getElementById("video-button"),
+      video_btn_close = document.getElementById("video-button_hidden");
+
+
 let album = document.getElementsByClassName("photo-contain");
-
-
-
 
 album_btn.addEventListener("click", ()=>{
     for(i=3; i<=album.length-1;i++){
-        album[i].classList.add("d-f"),
-        album[i].classList.remove("d-none"),
-        album_btn.classList.remove("d-f"),    
+        //album[i].classList.add("d-f"),
+        //album[i].classList.remove("d-none"),
+        album_btn.classList.remove("d-f"), 
         album_btn.classList.add("d-none"),
         album_btn_close.classList.remove("d-none"),
         album_btn_close.classList.add("d-f");
@@ -46,8 +83,8 @@ album_btn.addEventListener("click", ()=>{
 
 album_btn_close.addEventListener("click", ()=>{
     for(i=3; i<=album.length-1;i++){
-        album[i].classList.add("d-none"),
-        album[i].classList.remove("d-f"),
+        //album[i].classList.add("d-none"),
+        //album[i].classList.remove("d-f"),
         album_btn_close.classList.remove("d-f"),
         album_btn_close.classList.add("d-none"),
         album_btn.classList.add("d-f"),
@@ -55,62 +92,16 @@ album_btn_close.addEventListener("click", ()=>{
     }
     });
 
+video_btn.addEventListener("click", ()=>{
+        video_btn.classList.remove("d-f"), 
+        video_btn.classList.add("d-none"),
+        video_btn_close.classList.remove("d-none"),
+        video_btn_close.classList.add("d-f");
+    });
 
-    photo[0].addEventListener("click", ()=>{
-        galery_hidden.classList.remove("d-none");
-        galery_hidden.classList.add("d-f");
-        galery_hidden_photo[0].classList.add("d-f");
-        galery_hidden_photo[0].classList.remove("d-none");
-        });
-
-
-    photo[1].addEventListener("click", ()=>{
-        galery_hidden.classList.remove("d-none");
-        galery_hidden.classList.add("d-f");
-        galery_hidden_photo[1].classList.add("d-f");
-        galery_hidden_photo[1].classList.remove("d-none");
-        });
-    photo[2].addEventListener("click", ()=>{
-        galery_hidden.classList.remove("d-none");
-        galery_hidden.classList.add("d-f");
-        galery_hidden_photo[2].classList.add("d-f");
-        galery_hidden_photo[2].classList.remove("d-none");
-        });
-    photo[3].addEventListener("click", ()=>{
-        galery_hidden.classList.remove("d-none");
-        galery_hidden.classList.add("d-f");
-        galery_hidden_photo[3].classList.add("d-f");
-        galery_hidden_photo[3].classList.remove("d-none");
-        });
-    photo[4].addEventListener("click", ()=>{
-        galery_hidden.classList.remove("d-none");
-        galery_hidden.classList.add("d-f");
-        galery_hidden_photo[4].classList.add("d-f");
-        galery_hidden_photo[4].classList.remove("d-none");
-        });
-    photo[5].addEventListener("click", ()=>{
-        galery_hidden.classList.remove("d-none");
-        galery_hidden.classList.add("d-f");
-        galery_hidden_photo[5].classList.add("d-f");
-        galery_hidden_photo[5].classList.remove("d-none");
-        });
-
-
-galery_hidden.addEventListener("click", ()=>{
-    galery_hidden.classList.remove("d-f");
-    galery_hidden.classList.add("d-none");
-    galery_hidden_photo[0].classList.add("d-none");
-    galery_hidden_photo[0].classList.remove("d-f");
-    galery_hidden_photo[1].classList.add("d-none");
-    galery_hidden_photo[1].classList.remove("d-f");
-    galery_hidden_photo[2].classList.add("d-none");
-    galery_hidden_photo[2].classList.remove("d-f");
-    galery_hidden_photo[3].classList.add("d-none");
-    galery_hidden_photo[3].classList.remove("d-f");
-    galery_hidden_photo[4].classList.add("d-none");
-    galery_hidden_photo[4].classList.remove("d-f");
-    galery_hidden_photo[5].classList.add("d-none");
-    galery_hidden_photo[5].classList.remove("d-f");
-
-});
-*/
+video_btn_close.addEventListener("click", ()=>{
+        video_btn_close.classList.remove("d-f"),
+        video_btn_close.classList.add("d-none"),
+        video_btn.classList.add("d-f"),
+        video_btn.classList.remove("d-none"); 
+    });
